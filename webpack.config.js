04b5dfc126796477907f9c521e173304a35ccd37
Sergey Trasko <sergey.trasko@gmail.com>
2016-09-16 20:44:18 +0300

    Relative URLs

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist/static'),
         filename: 'bundle.js',
-        publicPath: '/static/'
+        publicPath: 'static/'
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
